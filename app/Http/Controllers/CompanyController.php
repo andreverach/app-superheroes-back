8dd117a9850de2d5c25082518f963a7d43a956f7
Andre Vera <andrevera.pe@gmail.com>
2022-02-11 23:03:58 -0500

    sesion 2 - laravel

--- app/Http/Controllers/CompanyController.php
+++ app/Http/Controllers/CompanyController.php
@@ -10,13 +10,35 @@ use App\Models\Company;
 
 class CompanyController extends Controller
 {
-    public function store(Request $request){
-        //codigo para guardar
-        Company::create([
-            'description' => $request->description,
-            'country' => $request->country
-        ]);
+    public function store(Request $request){      
+        //codigo para guardar        
 
-        return 'Información registrada';
+        $validated = $request->validate([
+            'description' => 'required|max:60',
+            'country' => 'required|max:60',
+        ],
+        [
+            'description.required' => 'La descripción no puede estar vacía.',
+            'description.max' => 'La descripción sobrepasa el límite de caracteres.',
+            'country.required' => 'El país no puede estar vacío.',
+            'country.max' => 'El país sobrepasa el límite de caracteres.',
+        ]);
+        
+        try {      
+            $company = Company::create([
+                'description' => $request->description,
+                'country' => $request->country
+            ]); 
+            //dd($company);
+            return response()->json([
+                'message' => 'La compañia ha sido registrada correctamente.',
+                'data' => $company
+            ], 201);
+        } catch (\Throwable $th) {
+            return response()->json([
+                'message' => 'Oopss, no se pudo registrar la información'                
+            ], 500);
+        }
     }
+    
 }
